61a9f93d6ed5d8f4e905813c54465529937f5a37
thiago733 <thiago733@gmail.com>
2022-08-16 09:21:57 -0400

    resolvendo o 12

--- estruturaSequencial/12.py
+++ estruturaSequencial/12.py
@@ -1,1 +1,5 @@
 #Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58 
+altura = float(input('Insira sua altura em m: '))
+homem = (72.7 * altura) - 58
+mulher = (62.1 * altura) - 44.7
+print(f'Seu peso ideal para homem é de {homem} e para mulher {mulher}')
